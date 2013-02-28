c6d150da068fffae518e32c02d13a5d8e7d45e01
Rasmus Erik <github-rasmuserik@solsort.dk>
2013-02-28 13:11:56 +0100

    update or clone depending on whether directory exists

--- backup.sh
+++ backup.sh
@@ -2,8 +2,15 @@ USER=rasmuserik
 install -d repos
 cd repos
 REPOS=`curl "https://github.com/$USER?tab=repositories" | grep '^    <a href="/rasmuserik/' | sed -e s/" *<[^>]*> *"//g`
+REPOS="$REPOS notes solsort rasmuserik.github.com erhversphd-application"
 
 for REPO in $REPOS 
 do
-    git clone git@github.com:$USER/$REPO.git
+    if [ -d $REPO ]; then
+        cd $REPO
+        git pull
+        cd ..
+    else
+        git clone git@github.com:$USER/$REPO.git
+    fi
 done
